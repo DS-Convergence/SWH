ed9390265060c2337ed0868a7ae49f572bba3615
Yesol Eom <yesoll0927@gmail.com>
2021-04-21 10:27:46 +0900

    sol-맵뷰추가

--- app/src/main/java/com/example/squirrelwarehouse/ItemLoc.kt
+++ app/src/main/java/com/example/squirrelwarehouse/ItemLoc.kt
@@ -7,5 +7,9 @@ class ItemLoc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_loc)
+
+        // https://webnautes.tistory.com/647 구글지도
+        // https://andro-jinu.tistory.com/entry/studio2 네이버지도
+
     }
 }
