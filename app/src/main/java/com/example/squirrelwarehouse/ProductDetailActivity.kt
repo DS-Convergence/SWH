9c2e5c3db89c06f9c2ffdb1f1b8c84b7e87ee1eb
yeeun6024 <yeeun6024>
2021-05-25 15:18:57 +0900

    예은 Product_detail.xml 데이터 가져오기

--- app/src/main/java/com/example/squirrelwarehouse/ProductDetailActivity.kt
+++ app/src/main/java/com/example/squirrelwarehouse/ProductDetailActivity.kt
@@ -1,11 +1,18 @@
 package com.example.squirrelwarehouse
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
+import android.util.Log
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.GlideDrawable
+import com.bumptech.glide.request.target.Target
+import com.example.squirrelwarehouse.models.Product
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+
 
 class ProductDetailActivity : AppCompatActivity() {
 
@@ -18,6 +25,11 @@ class ProductDetailActivity : AppCompatActivity() {
     private lateinit var tvProdDetail : TextView
     private lateinit var tvMoney : TextView
     private var state = false;
+    private lateinit var img : ImageView
+
+    private var firestore : FirebaseFirestore? = null
+    private var storage : FirebaseStorage? = null
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,14 +41,51 @@ class ProductDetailActivity : AppCompatActivity() {
         tvStatus = findViewById(R.id.tv_time)
         tvUser = findViewById(R.id.tv_user)
         tvUserLocation = findViewById(R.id.tv_uesrLocation)
-
         tvProdDetail = findViewById(R.id.tv_prodDetail)
-
         tvMoney = findViewById(R.id.tv_money)
+        img = findViewById(R.id.img_prod)
 
         var btnHeart : ImageButton = findViewById(R.id.btn_heart)
         val btnReport : ImageButton = findViewById(R.id.btn_report)
         val btnSubmenu : TextView = findViewById(R.id.btn_submenu)
+
+        firestore = FirebaseFirestore.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        firestore?.collection("Product")?.document("K4cXhtjOFzbfQ2KtXGSg")?.get()?.addOnCompleteListener {
+            task ->
+            if(task.isSuccessful) { // 데이터 가져오기를 성공하면
+                var product = task.result.toObject(Product::class.java)
+                tvProdName.text = product?.ProductName
+                tvProdCategory.text = product?.Category
+                tvTime.text = product?.UploadTime
+                //tvStatus.text = product?
+                tvUser.text = product?.UserName
+                tvUserLocation.text = product?.Region.toString()
+                tvProdDetail.text = product?.ProductDetail
+                // 보증금
+                // 대여료
+
+                // 사진 불러오기
+                var storageRef = storage?.reference?.child("product")?.child(product?.ImageURI.toString())
+                storageRef?.downloadUrl?.addOnSuccessListener { uri ->
+                    Glide.with(applicationContext)
+                            .load(uri)
+                            .into(img)
+                    Log.v("IMAGE","Success")
+                }?.addOnFailureListener { //이미지 로드 실패시
+                    Toast.makeText(applicationContext, "실패", Toast.LENGTH_SHORT).show()
+                    Log.v("IMAGE","failed")
+                }
+
+
+                // 현재 사용자가 글쓴이면 더보기 보이고 아니면 안보이도록
+
+
+            }
+        }
+
 
 
         btnHeart.setOnClickListener() {
