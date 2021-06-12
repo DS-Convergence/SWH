b178a7c98593b5dff5339efd39ec8bad1f96350c
Yesol Eom <yesoll0927@gmail.com>
2021-06-12 16:50:13 +0900

    sol&yeeun_메인페이지 추천알고리즘

--- app/src/main/java/com/example/squirrelwarehouse/MainPageActivity.kt
+++ app/src/main/java/com/example/squirrelwarehouse/MainPageActivity.kt
@@ -8,25 +8,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.example.squirrelwarehouse.models.Favorite
 import com.example.squirrelwarehouse.models.Product
+import com.example.squirrelwarehouse.models.UserModelFS
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.storage.FirebaseStorage
-import kotlinx.android.synthetic.main.listview.view.*
-import kotlinx.android.synthetic.main.listview.view.thumb
-import kotlinx.android.synthetic.main.listview_form.*
 import kotlinx.android.synthetic.main.main_itemview.view.*
 import kotlinx.android.synthetic.main.main_page.*
 
 
 class MainPageActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
-    private var firestore : FirebaseFirestore? = null
+    private var firestore = FirebaseFirestore.getInstance()
     private var storage : FirebaseStorage? = null
     private lateinit var auth: FirebaseAuth
 
@@ -111,7 +108,9 @@ class MainPageActivity : AppCompatActivity() {
         // 업데이트
         // 리사이클러뷰: updateView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         // var updateAdapter : MainViewAdapter = MainViewAdapter()
-        //var updateList : ArrayList<HashMap<String, String>> = dataInArray()
+        // var updateList : ArrayList<HashMap<String, String>> = dataInArray()
+
+        // 추천테스트: var updateList = UserBasedRcmd("user_ifbnimzN2RM61ZfbfeJ48ZBdu9j2").getRcmd()
 
         // updateAdapter.items.clear()
         // updateView.adapter = updateAdapter
@@ -194,11 +193,9 @@ class MainPageActivity : AppCompatActivity() {
 
         // 추천 - 더보기
         var moreRcmd = moreRcmd
-        moreRcmd.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                // listView로 연결
-            }
-        })
+        moreRcmd.setOnClickListener {
+            Log.v("RcmdList", UserBasedRcmd("user_ifbnimzN2RM61ZfbfeJ48ZBdu9j2").getRcmd().toString())
+        }
 
 
 
@@ -217,7 +214,6 @@ class MainPageActivity : AppCompatActivity() {
 
     fun dataInArray(): ArrayList<HashMap<String, String>> {
 
-        firestore = FirebaseFirestore.getInstance()
         val data = firestore?.collection("Product")
         val itemList = arrayListOf<HashMap<String, String>>()
 
@@ -237,7 +233,6 @@ class MainPageActivity : AppCompatActivity() {
 
     fun dataLoading() : ArrayList<Product> {
         var products: ArrayList<Product> = arrayListOf()
-        firestore = FirebaseFirestore.getInstance()
         firestore?.collection("Product")?.orderBy("uploadTime", Query.Direction.DESCENDING)?.limit(3)
                 ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                     products.clear()
@@ -344,6 +339,148 @@ class MainPageActivity : AppCompatActivity() {
 
             }
 
+        }
+    }
+
+    inner class UserBasedRcmd(private var userId : String) {
+        var user : String? = userId
+        // var userIndex = 0
+        // var data = arrayOf(datas)
+        lateinit var sim: ArrayList<ArrayList<Double>>    // 유사도
+        lateinit var users : ArrayList<String>
+        lateinit var product : ArrayList<String>
+        lateinit var rcmdList : ArrayList<String>   // 물건 정보 들어갈 배열
+
+        fun UserBased(userId: String) {
+            this.user = userId
+            var data : ArrayList<ArrayList<Int>> = getData()    // product와 favorite 비교해서 선호데이터 받기
+
+            // 유사도 행렬
+            for (i in 0..users.size-1) {
+                lateinit var simArr : ArrayList<Double>
+                for(j in 0..users.size-1) {
+                    if(i==j)
+                        simArr.add(0.0)
+                    else
+                        simArr.add(cosineSimilarity(data.get(i),data.get(j)))
+                }
+                sim.add(simArr)
+            }
+
+            // 현재 유저와 다른 유저와의 유사도만 뽑아내기
+            var index1 = 0
+            while (index1 < users.size) {
+                if (users.get(index1) == user) //userIndex = i;
+                    break
+                index1++
+            }
+
+            // 유사도 제일 높은 사람의 정보 출력하기
+            var max: Double = sim.get(index1).get(0)
+            var index2 = 0
+            for (j in 0..users.size-1) {
+                if (max < sim.get(index1).get(j)) {
+                    max = sim.get(index1).get(j)
+                    index2 = j
+                }
+            }
+
+            // 나랑 유사도가 가장 높은 사람의 물건 선호도 출력
+            /*for (dd in data.get(index2)) {
+                print("$dd ")
+            }
+             */
+
+            // 나의 물건 선호도 출력
+            /*for (dd in data.get(index1)) {
+                print("$dd ")
+            }
+             */
+
+            // 내가 보지 않았지만, 상대는 관심있는 물건, 인덱스 출력
+            for (j in 0..data.get(index1).size-1) {
+                if (data.get(index1).get(j) == 0 && data.get(index2).get(j) != 0) {
+                    rcmdList.add(product.get(j))
+                }
+            }
+        }
+
+        fun getRcmd() : ArrayList<String> {
+            rcmdList.add("hi")
+            return rcmdList
+        }
+
+        private fun getData() : ArrayList<ArrayList<Int>> {
+            // 파이어베이스에서 데이터 불러와야 함
+            // 지금은 일단 더미 데이터로 해보기
+            // favorite 데이터랑 product 데이터 가져와야 함
+            // data, users
+            // sim = Array(data.size) { DoubleArray(data.size) }
+            lateinit var productArr : ArrayList<String>
+            firestore?.collection("Product")?.orderBy("uploadTime", Query.Direction.DESCENDING)?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                productArr.clear()
+                if (querySnapshot == null) return@addSnapshotListener
+
+                // 데이터 받아오기
+                for (snapshot in querySnapshot!!.documents) {
+                    var item = snapshot.toObject(Product::class.java)
+                    productArr.add(item!!.userId.toString() + "_" + item!!.uploadTime.toString())
+                }
+            }
+            product = productArr
+
+            lateinit var userArr : ArrayList<String>
+            firestore?.collection("Users")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                userArr.clear()
+                if (querySnapshot == null) return@addSnapshotListener
+
+                // 데이터 받아오기
+                for (snapshot in querySnapshot!!.documents) {
+                    var item = snapshot.toObject(UserModelFS::class.java)
+                    userArr.add(item!!.uid.toString())
+                }
+            }
+            users = userArr
+
+            lateinit var favArr : ArrayList<ArrayList<String>>
+            firestore?.collection("Favorite")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                favArr.clear()
+                if (querySnapshot == null) return@addSnapshotListener
+
+                // 데이터 받아오기
+                for (snapshot in querySnapshot!!.documents) {
+                    var item = snapshot.toObject(Favorite::class.java)
+                    favArr.add(item!!.products as java.util.ArrayList<String>)
+                }
+            }
+
+            lateinit var data : ArrayList<ArrayList<Int>>   // 전체 유저의 선호도 데이터
+            lateinit var arr : ArrayList<Int>   // 한 사람의 선호도 데이터
+            for(i in 0..favArr.size-1) {
+                arr.clear()
+                for(j in 0..productArr.size-1) {
+                    if(favArr.get(i).contains(productArr.get(j)))
+                        arr.add(1)
+                    else
+                        arr.add(0)
+                }
+                data.add(arr)
+            }
+
+            return data
+        }
+
+        private fun cosineSimilarity(user1: ArrayList<Int>, user2: ArrayList<Int>): Double {
+            // 코사인 유사도 계산
+            var sum = 0.0
+            var sum1 = 0.0
+            var sum2 = 0.0
+            for (i in 0..user1.size-1) {
+                sum += (user1[i] * user2[i]).toDouble()
+                sum1 += Math.pow(user1[i].toDouble(), 2.0)
+                sum2 += Math.pow(user2[i].toDouble(), 2.0)
+            }
+            return sum / (Math.sqrt(sum1) * Math.sqrt(sum2))
         }
     }
 
