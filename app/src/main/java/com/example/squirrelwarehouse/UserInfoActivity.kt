837838b8add22d02fd13342b85d38a16aed8f4d0
pabaep <sewon225@gmail.com>
2021-07-03 21:19:42 +0900

    ..

--- app/src/main/java/com/example/squirrelwarehouse/UserInfoActivity.kt
+++ app/src/main/java/com/example/squirrelwarehouse/UserInfoActivity.kt
@@ -34,7 +34,46 @@ class UserInfoActivity : AppCompatActivity() {
         user_listview.layoutManager = layoutManager
 
         user_listview.adapter = ItemAdapter2()
-        //TODO 클릭한 사용자 받는 걸로 수정하기
+
     }
+/*    inner class ItemAdapter2 : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var itemList: ArrayList<Item2> = arrayListOf()
+
+        init {
+            firestore?.collection("Product")?.whereEqualTo("userId", auth.currentUser?.uid)
+                    ?.get()?.addOnSuccessListener { documents ->
+                        itemList.clear()
+                        for (doc in documents) {
+                            title = doc?.data?.get("productName").toString()
+                            time = doc?.data?.get("uploadTime").toString()
+                            detail = doc?.data?.get("productDetail").toString()
+                            item_pic = doc?.data?.get("imageURI").toString()
+                            var item = Item2(title, time, detail, item_pic)
+                            itemList.add(item!!)
+                        }
+                        notifyDataSetChanged()//새로고침
+                    }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.listview, parent, false)
+            return ViewHolder(view)
+        }
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        }
+
+        override fun getItemCount(): Int {
+            return itemList.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var viewHolder = (holder as ViewHolder).itemView
+            viewHolder.titleTV.text = itemList[position].title
+            viewHolder.timeTV.text = itemList[position].time
+            viewHolder.detailTV.text = itemList[position].detail
+        }
+        //TODO 사용자 별로 띄우는 거 보여주기(current말고), 그외 연결도 하기
+
+    }*/
 
 }
