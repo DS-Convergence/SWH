d9d620152797f0881fb4a7abc6091d071763883a
yeeun6024 <yeeun6024>
2021-09-01 20:10:40 +0900

    예은 조회수 성공 & 예솔 코드 오류 수정

--- app/src/main/java/com/example/squirrelwarehouse/models/Product.kt
+++ app/src/main/java/com/example/squirrelwarehouse/models/Product.kt
@@ -4,4 +4,4 @@ import com.google.firebase.firestore.GeoPoint
 
 // 물건 정보
 data class Product(var userId : String? = null, var userName : String? = null, var productName : String? = null, var category : String? = null, var categoryHobby : String? = null, var productDetail : String? = null,
-                   var imageURI : String? = null, var deposit : String? = null, var rentalFee : String? = null, var uploadTime : String? = null, var region : GeoPoint? = null, var status : String? = null)
+                   var imageURI : String? = null, var deposit : String? = null, var rentalFee : String? = null, var uploadTime : String? = null, var region : GeoPoint? = null, var status : String? = null, var view : Int? = 0)
