6cc11ece2ccc2859485ff4eb5969d5b872589a15
pabaep <sewon225@gmail.com>
2021-09-05 20:43:58 +0900

    파워람쥐 신청화면 완성

--- app/src/main/java/com/example/squirrelwarehouse/PowerRamgiApplicationActivity.kt
+++ app/src/main/java/com/example/squirrelwarehouse/PowerRamgiApplicationActivity.kt
@@ -1,11 +1,23 @@
 package com.example.squirrelwarehouse
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import kotlinx.android.synthetic.main.activity_power_ramgi_application.*
 
 class PowerRamgiApplicationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_power_ramgi_application)
+        back_btn.setOnClickListener {
+            finish()
+        }
+        powerRamgi_submit_btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://forms.gle/ybZ1ZtLXfVDkJV8e8") )
+            startActivity(intent)
+        }
     }
+
 }
