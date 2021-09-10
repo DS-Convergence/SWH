2831912c90a386c8582724f0320537dcba81bb31
pabaep <sewon225@gmail.com>
2021-09-10 17:04:59 +0900

    파워람쥐ㅜ정, 신고 페이지 수정, 기타 UI수정

--- app/src/main/java/com/example/squirrelwarehouse/ReportActivity.kt
+++ app/src/main/java/com/example/squirrelwarehouse/ReportActivity.kt
@@ -1,11 +1,23 @@
 package com.example.squirrelwarehouse
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_report.*
 
 class ReportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+
+        back_btn.setOnClickListener {
+            finish()
+        }
+        //신고 좀 더 보완해야 할거 같음
+        report_form_btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://forms.gle/yKHEgjgP3AxHxSqf8"))
+            startActivity(intent)
+        }
     }
 }
