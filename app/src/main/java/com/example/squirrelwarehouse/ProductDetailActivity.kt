01b93ad4dc8fc1201c130ee40bb10b4af694821e
yeeun6024 <yeeun6024>
2021-09-16 15:43:59 +0900

    예은 ProductDetailActivity.kt 코드 수정

--- app/src/main/java/com/example/squirrelwarehouse/ProductDetailActivity.kt
+++ app/src/main/java/com/example/squirrelwarehouse/ProductDetailActivity.kt
@@ -24,18 +24,7 @@ import kotlin.math.ln
 
 class ProductDetailActivity : AppCompatActivity() {
 
-    private lateinit var tvProdName : TextView
-    private lateinit var tvProdCategory : TextView
-    private lateinit var tvProdCategoryHobby : TextView
-    private lateinit var tvTime : TextView
-    private lateinit var tvStatus : TextView
-    private lateinit var tvUser : TextView
-    private lateinit var tvUserLocation : TextView
-    private lateinit var tvProdDetail : TextView
-    private lateinit var tvDeposit : TextView
-    private lateinit var tvRentalfee : TextView
-    private var state = false;
-    private lateinit var img : ImageView
+    private var state = false; // 물건 거래 상황
 
     private var firestore : FirebaseFirestore? = null
     private var storage : FirebaseStorage? = null
@@ -44,8 +33,6 @@ class ProductDetailActivity : AppCompatActivity() {
     private lateinit var userid : String
 
     private lateinit var prod : String  // 물건 id
-
-    private lateinit var prodUser : String  // 물건 주인 id
 
     private var start: Long = 0
     private var stop: Long = 0
@@ -57,35 +44,22 @@ class ProductDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.product_detail)
 
-        tvProdName = findViewById(R.id.tv_prodName)
-        tvProdCategory = findViewById(R.id.tv_prodCategory)
-        tvProdCategoryHobby = findViewById(R.id.tv_prodCategoryHobby)
-        tvTime = findViewById(R.id.tv_time)
-        tvStatus = findViewById(R.id.tv_status)
-        tvUser = findViewById(R.id.tv_user)
-        tvUserLocation = findViewById(R.id.tv_userLocation)
-        tvProdDetail = findViewById(R.id.tv_prodDetail)
-        tvDeposit = findViewById(R.id.tv_deposit)
-        tvRentalfee = findViewById(R.id.tv_rentalfee)
-        img = findViewById(R.id.img_prod)
-
-        var btnHeart : ImageButton = findViewById(R.id.btn_heart)
-        val btnReport : ImageButton = findViewById(R.id.btn_report)
-        val btnSubmenu : TextView = findViewById(R.id.btn_submenu)
-        val btnChat : Button = findViewById(R.id.btn_chat)
-
         firestore = FirebaseFirestore.getInstance()
         storage = FirebaseStorage.getInstance()
         auth = FirebaseAuth.getInstance()
 
 
-        // intent로 물건 id 정보 넘겨 받아야함!
+        // intent로 물건 id 정보 넘겨 받아야함
         val intent = intent
         prod = intent.getStringExtra("data").toString()
 
-        firestore?.collection("Product")?.document(prod)?.get()?.addOnCompleteListener { // 넘겨온 물건 id를 넣어주면 됨.
+
+        // 넘겨 받은 물건 id를 통해 데이터 가져오기
+        firestore?.collection("Product")?.document(prod)?.get()?.addOnCompleteListener {
                 task ->
             if(task.isSuccessful) { // 데이터 가져오기를 성공하면
+
+                // product_detail.xml에 데이터 넣기
                 var product = task.result.toObject(Product::class.java)
                 tv_prodName.text = product?.productName
                 tv_prodCategory.text = product?.category
@@ -94,18 +68,26 @@ class ProductDetailActivity : AppCompatActivity() {
                 tv_user.text = product?.userName
                 tv_userLocation.text = "위치 정보 오류"
                 tv_prodDetail.text = product?.productDetail
-
                 tv_bar_prodName.text = product?.productName
 
+
+                // 물건 주인 id 저장
+                userid = product?.userId.toString()
+
+
+                // 조회수 가져오기
                 viewcount = product?.view
+
 
                 // 시간 데이터 형식 변경
                 var sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
                 var date = sdf.parse(product?.uploadTime)
-                sdf = SimpleDateFormat("yy년 MM월 dd일 HH시 mm분")
+                sdf = SimpleDateFormat("yyyy.MM.dd HH:mm")
                 var dateStr = sdf.format(date)
                 tv_time.text = dateStr
 
+
+                // 위도, 경도 데이터 > 한글 주소
                 var geocoder = Geocoder(this)
                 var list : List<Address>? = null
                 if(product?.region != null)
@@ -121,37 +103,31 @@ class ProductDetailActivity : AppCompatActivity() {
 
                         var str = list.get(0).toString().split(" ")
                         var str2 = str[1] + " " + str[2] + " " + str[3]
-                        if(str2.length <= 20)
+
+                        if(str2.length <= 40)  // 가끔가다 list.get(0) 전부가 뜨는 경우가 있음. 그것을 방지하기 위함
                             tv_userLocation.text = str2
                     }
                 }
 
+
                 // 보증금
-                if(product?.deposit.equals("")) { // 체크가 안되어 있거나
-                    tv_deposit.text = "보증금: 0원"
-                }
-                else {
-                    tv_deposit.text = "보증금: " + product?.deposit + "원"
-                }
+                if(product?.deposit.equals("")) tv_deposit.text = "보증금: 0원"   // 값이 비어있으면 0원
+                else tv_deposit.text = "보증금: " + product?.deposit + "원"
+
 
                 // 대여료
-                if(product?.rentalFee.equals("")) {
-                    tv_rentalfee.text = "대여료: 0원"
-                }
-                else {
-                    tv_rentalfee.text = "대여료: " + product?.rentalFee + "원"
-                }
+                if(product?.rentalFee.equals("")) tv_rentalfee.text = "대여료: 0원"  // 값이 비어있으면 0원
+                else tv_rentalfee.text = "대여료: " + product?.rentalFee + "원"
 
-
-                userid = product?.userId.toString()
 
                 // 사진 불러오기
                 var storageRef = storage?.reference?.child("product")?.child(product?.imageURI.toString())
                 storageRef?.downloadUrl?.addOnSuccessListener { uri ->
                     Glide.with(applicationContext)
                         .load(uri)
-                        .into(img)
+                        .into(img_prod)
                     Log.v("IMAGE","Success")
+
 
                     // 좋아요 리스트에 있는지 데이터 가져옴
                     firestore?.collection("Favorite")?.document(auth.currentUser!!.uid)?.get()?.addOnCompleteListener {
@@ -159,47 +135,43 @@ class ProductDetailActivity : AppCompatActivity() {
                         if(task.isSuccessful){
                             var favorite = task.result.toObject(Favorite::class.java)
                             var array = favorite?.products
-                            if (array != null) { // array가 null이 아니면
-                                for(product in array) { // array에 현재 보고 있는 물건이 있는지 판단한다.
-                                    if(product.equals(prod)) {
-                                        state = true;
-                                        btnHeart.setImageResource(R.drawable.heart_green) // array에 있으면 초록하트로.
-                                    }
-                                    Log.v("PRODUCTS",product)
-                                }
 
+                            if (array != null) { // array가 null이 아니면
+                                if(array.contains(prod)) { // array에 현재 보고 있는 물건이 있는지 판단
+                                    state = true;
+                                    btn_heart.setImageResource(R.drawable.heart_green) // array에 있으면 초록하트로.
+                                }
                             }
                             Log.v("HEART","Success"+ state.toString())
                         }else {
                             Log.v("HEART","Failed")
                         }
                     }
-
-                }?.addOnFailureListener { //이미지 로드 실패시
-                    //Toast.makeText(applicationContext, "실패", Toast.LENGTH_SHORT).show()
+                }?.addOnFailureListener {  //이미지 로드 실패시
                     Log.v("IMAGE","failed")
                 }
 
 
-                // 현재 사용자가 글쓴이면 더보기 보이고 아니면 안보이도록
                 // 더보기, 찜하기, 채팅하기, 신고버튼
+                // 현재 사용자가 글쓴이면 더보기 보이고 아니면 안보이도록
                 if(product?.userId.equals(auth.currentUser!!.uid)) { //
-                    btnSubmenu.visibility = View.VISIBLE
-                    btnHeart.visibility = View.GONE
-                    btnChat.visibility = View.GONE
+                    btn_submenu.visibility = View.VISIBLE
+                    btn_heart.visibility = View.GONE
+                    btn_chat.visibility = View.GONE
                     btn_report.visibility = View.GONE
                 }
                 else {
-                    btnSubmenu.visibility = View.GONE
-                    btnHeart.visibility = View.VISIBLE
-                    btnChat.visibility = View.VISIBLE
+                    btn_submenu.visibility = View.GONE
+                    btn_heart.visibility = View.VISIBLE
+                    btn_chat.visibility = View.VISIBLE
                     btn_report.visibility = View.VISIBLE
                 }
 
+
                 // 대여완료된 물건이면 채팅하기 버튼 숨김
                 if(product?.status.equals("대여 종료")) {
-                    btnChat.visibility = View.INVISIBLE
-                    //btnHeart.visibility = View.GONE
+                    btn_chat.visibility = View.INVISIBLE
+                    //btn_heart.visibility = View.GONE
                     tv_closed.visibility = View.VISIBLE
                 }
 
@@ -207,13 +179,13 @@ class ProductDetailActivity : AppCompatActivity() {
         }
 
 
-
-        btnHeart.setOnClickListener() {
+        // 찜버튼
+        btn_heart.setOnClickListener() {
             // 디폴트는 하얀 하트, 관심상품 등록했으면 초록 하트, 데이터베이스와 연동필요
             // Firebase로 안드로이드 sns 앱 만들기 195-197쪽 참고
-            // 맨처음에 계정을 만들 때 Favorite 데이터도 함께 생성시켜야할 것 같음.
-            if(!state) {
-                btnHeart.setImageResource(R.drawable.heart_green)
+
+            if(!state) { // 물건을 찜할 경우
+                btn_heart.setImageResource(R.drawable.heart_green)
                 state = true;
 
                 // Favorite 컬렉션에 있는 uid를 이용
@@ -239,17 +211,12 @@ class ProductDetailActivity : AppCompatActivity() {
                                 Log.v("HEART","Update Failed")
                             }
                         }
-
-
-                        //Log.v("HEART","Success"+ state.toString())
-                    }else {
-                        //Log.v("HEART","Failed")
                     }
                 }
 
             }
-            else {
-                btnHeart.setImageResource(R.drawable.heart_white)
+            else { // 이미 찜한 물건을 해제할 경우
+                btn_heart.setImageResource(R.drawable.heart_white)
                 state = false;
 
                 firestore?.collection("Favorite")?.document(auth.currentUser!!.uid)?.get()?.addOnCompleteListener {
@@ -272,44 +239,45 @@ class ProductDetailActivity : AppCompatActivity() {
                                 Log.v("HEART","Update Failed")
                             }
                         }
-
-
-                        //Log.v("HEART","Success"+ state.toString())
-                    }else {
-                        //Log.v("HEART","Failed")
                     }
                 }
-
             }
         }
 
 
-        btnReport.setOnClickListener() {
-            // 신고 페이지로 이동
-            val intent = Intent(this, ReportActivity::class.java)
+        // 신고버튼
+        btn_report.setOnClickListener() {
+            val intent = Intent(this, ReportActivity::class.java)  // 신고 페이지로 이동
             startActivity(intent)
         }
 
-        btnSubmenu.setOnClickListener {
-            // ProductDetailSubmenu로 이동
-            val intent = Intent(this, ProductDetailSubmenuActivity::class.java)
+
+        // 서브메뉴버튼
+        btn_submenu.setOnClickListener {
+            val intent = Intent(this, ProductDetailSubmenuActivity::class.java)  // ProductDetailSubmenu로 이동
             intent.putExtra("ProductID", prod)
             startActivityForResult(intent, 0)
         }
 
-        btnChat.setOnClickListener {
+
+        // 채팅버튼
+        btn_chat.setOnClickListener {
             val intent = Intent(this, ChatLogActivity::class.java)
             intent.putExtra("ProductID",prod)
             intent.putExtra("UserId",userid)
             startActivity(intent)
         }
 
-        tvUser.setOnClickListener {
-            val intent = Intent(this, UserInfoActivity::class.java)
+
+        // 유저이름
+        tv_user.setOnClickListener {
+            val intent = Intent(this, UserInfoActivity::class.java)  // 물건주인의 마이페이지로 이동
             intent.putExtra("UserId",userid)
             startActivity(intent)
         }
 
+
+        // 뒤로가기 버튼
         back_btn.setOnClickListener {
             finish()
         }
@@ -319,44 +287,35 @@ class ProductDetailActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        //finish()
-        // 변경된 데이터를 불러오기 위해 자신의 액티비티를 다시 호출
-        /*
-        var intent = Intent(this, ProductDetailActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        startActivity(intent)
-        finish()
-*/
         if(requestCode==0) {
-            if(resultCode == Activity.RESULT_OK) {
-                val intent = Intent()
-                //prod = data!!.getStringExtra("TextOut").toString()
-                var intent2 = Intent(this, ProductDetailActivity::class.java)
-                intent2.putExtra("data",data!!.getStringExtra("TextOut").toString())
-                intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                startActivity(intent2)
+            if(resultCode == Activity.RESULT_OK) {  // 게시글 수정 후 실행되는 코드
+                // 변경된 데이터를 불러오기 위해 자신의 액티비티를 다시 호출
+                var intent = Intent(this, ProductDetailActivity::class.java)
+                intent.putExtra("data",data!!.getStringExtra("TextOut").toString())
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
                 finish()
             }
-            else if(resultCode == Activity.CONTEXT_INCLUDE_CODE) {
+            else if(resultCode == Activity.CONTEXT_INCLUDE_CODE) {  // 게시글 삭제 후 실행되는 코드
                 finish()
-            }
-            else {
-                //finish()
             }
         }
-
-
     }
 
     override fun onStart() {
         super.onStart()
+
+        // 페이지에 처음 들어온 시각 저장
         start = System.currentTimeMillis()
         Log.v("MainActivity", "onStart() "+start)
     }
 
     override fun onStop() {
         super.onStop()
+
+        // 페이지가 꺼지거나 다른 화면에 의해 가려진 시각 저장
         stop = System.currentTimeMillis()
+        // 페이지 본 시간 저장
         total += (stop - start)
         Log.v("MainActivity", "onStop() "+(stop - start))
 
@@ -366,20 +325,14 @@ class ProductDetailActivity : AppCompatActivity() {
         super.onDestroy()
         Log.v("MainActivity", "onDestroy() " + (total/1000) + "초")
 
-        //auth = FirebaseAuth.getInstance()
-        //var map = mutableMapOf<String,Int>()
-        // prod 오류나면 여기서 정의 해줘야함.
-        //map.put(prod, (total/1000).toInt())
-        //var staytime = StayTime(map)
-
         // 데이터 베이스에 물건 본 시간 넣는 코드
         firestore?.collection("StayTime")?.document(auth.currentUser!!.uid)?.get()?.addOnCompleteListener {
             task ->
             if(task.isSuccessful) {
-
                 var staytime = task.result.toObject(StayTime::class.java)
                 var stmap = (staytime?.products as Map<String,Int>).toMutableMap()
                 var trmap = (staytime?.transform as Map<String,Int>).toMutableMap()
+
                 if(stmap.containsKey(prod)) { // 이미 있는 물건, 즉 이미 본 물건이라면 본 시간을 업데이트
                     stmap[prod] = stmap.get(prod)!!.toInt() + (total/1000).toInt()
                 }
@@ -393,23 +346,11 @@ class ProductDetailActivity : AppCompatActivity() {
 
 
                 // 원데이터를 로그변환하는 코드
-                if(stmap.containsKey(prod)) { // 원데이터가 있는 경우에만, 1이상인 경우에만 변환적용
+                if(stmap.containsKey(prod)) { // 원데이터가 있는 경우에만, 데이터는 1이상인 데이터만 존재하기 때문에 음수값 나오지 않음.
                     trmap[prod] = ln(stmap.get(prod)!!.toDouble()).toInt()
                     map["transform"] = trmap
                 }
 
-
-                /*
-                // 로그변환 위한 코드임. 일시적으로 필요한 코드. 사용 후 삭제
-                var trans = mutableMapOf<String,Int>()
-                for(p in stmap) {
-                    trans.put(p.key, ln(p.value.toDouble()).toInt())
-                    Log.v("trans",p.key + " " + ln(p.value.toDouble()).toInt())
-                }
-
-                map["transform"] = trans
-                // 사용후 삭제
-                */
 
                 // 머무른 시간과 조회수 적용
                 if(!auth.currentUser!!.uid.equals(userid)){  // 물건 주인이 현재 사용자이면 머무른 시간과 조회수 모두 적용 x
@@ -419,14 +360,13 @@ class ProductDetailActivity : AppCompatActivity() {
                             Log.v("staytime","Update Success")
 
                             var map = mutableMapOf<String?,Any?>()
-                            map["view"] = viewcount!!.toInt() + 1
+                            map["view"] = viewcount!!.toInt() + 1  // 조회수 1 추가
                             firestore?.collection("Product")?.document(prod)?.update(map)?.addOnCompleteListener {
                                 task ->
                                 if(task.isSuccessful) {
                                     Log.v("viewcount","Update Success")
                                 }
                             }
-
                         }
                         else {
                             Log.v("staytime","Update Failed")
