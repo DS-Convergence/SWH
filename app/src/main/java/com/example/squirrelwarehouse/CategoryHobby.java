b6c97c1d10257b1a72d8e1afcb731456420ace8b
yeeun6024 <yeeun6024>
2021-10-27 22:57:21 +0900

    예은 카테고리(자바파일) 변경 완료

--- app/src/main/java/com/example/squirrelwarehouse/CategoryHobby.java
+++ app/src/main/java/com/example/squirrelwarehouse/CategoryHobby.java
@@ -16,67 +16,125 @@ public class CategoryHobby {
     private void setDataInit() {
 
         // 공부 > 0
-        map.put("overskirt",0);
-
-
+        map.put("stationery",0);
+        map.put("pencil case",0);
+        map.put("printer",0);
+        map.put("book scanner",0);
 
 
         // 게임 > 1
-        map.put("power drill",1);
-
-
+        map.put("keyboard",1);
+        map.put("console game",1);
+        map.put("portable video game",1);
+        map.put("gaming chair",1);
+        map.put("gaming mouse",1);
+        map.put("monitor",1);
+        map.put("charger",1);
 
 
         // 독서 > 2
-        map.put("bakery",2);
-
-
-
+        map.put("reading table",2);
+        map.put("magnifier",2);
+        map.put("book",2);
+        map.put("bookmark",2);
+        map.put("thimble",2);
+        map.put("stand",2);
 
 
         // 문화/예술 > 3
-        map.put("balloon",3);
-
-
-
+        map.put("frame",3);
+        map.put("opera glass",3);
+        map.put("fabric poster",3);
+        map.put("photo printer",3);
+        map.put("printer",3);
+        map.put("tripod",3);
+        map.put("camera lens",3);
+        map.put("camera bag",3);
+        map.put("filming light",3);
 
 
         // 미술/공예 > 4
-        map.put("balance beam",4);
-
-
-
+        map.put("brush",4);
+        map.put("paints",4);
+        map.put("pallet",4);
+        map.put("easel",4);
+        map.put("apron",4);
+        map.put("cutting mat",4);
+        map.put("tablet",4);
+        map.put("gesso",4);
+        map.put("pencil",4);
+        map.put("utility knife",4);
+        map.put("tape",4);
+        map.put("carving knife",4);
+        map.put("resin lamp",4);
+        map.put("sewing machine",4);
 
 
         // 요리 > 5
-        map.put("plow",5);
-
-
+        map.put("kettle",5);
+        map.put("coffee kettle",5);
+        map.put("hand mixer",5);
+        map.put("weighting scale",5);
+        map.put("dripper",5);
+        map.put("spatula",5);
+        map.put("cookie cutter",5);
+        map.put("mixing bowl",5);
+        map.put("blender",5);
+        map.put("cutting board",5);
+        map.put("kitchen knife",5);
+        map.put("kitchen scissors",5);
+        map.put("knife sharpener stick",5);
+        map.put("whisk",5);
+        map.put("silicone mold",5);
+        map.put("knife sharpener",5);
+        map.put("analogue weighting scale",5);
 
 
         // 원예 > 6
-        map.put("abacus",6);
-
-
+        map.put("shovel",6);
+        map.put("ratan basket",6);
+        map.put("homie",6);
+        map.put("watering can",6);
+        map.put("sickle",6);
 
 
         // 운동/스포츠 > 7
-        map.put("slot",7);
-
-
+        map.put("bicycle",7);
+        map.put("helmet",7);
+        map.put("stick",7);
+        map.put("skateboard",7);
+        map.put("protector",7);
+        map.put("fishinggrob",7);
+        map.put("golfclub",7);
+        map.put("snorkeling",7);
+        map.put("shoes",7);
+        map.put("quickboard",7);
+        map.put("ski",7);
+        map.put("inlineskate",7);
+        map.put("skates",7);
+        map.put("ball",7);
 
 
         // 음악 > 8
-        map.put("Old English sheepdog",8);
+        map.put("turntable",8);
+        map.put("cd album",8);
+        map.put("speaker",8);
+        map.put("headset",8);
+        map.put("earphone",8);
+        map.put("vinyl",8);
+        map.put("violin",8);
+        map.put("ukulele",8);
 
 
         // 펫 > 9
-        map.put("Old English sheepdog",9);
+        map.put("water fountain",9);
+        map.put("pet wheel",9);
+        map.put("bird cage",9);
+        map.put("automatic feeder",9);
+
 
 
         // 기타 > 10
-        map.put("Old English sheepdog",10);
-
 
 
 
